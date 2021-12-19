32469a648e28dc132614d7e3c357947694d7ffdf
linkenvashistha <vashisthalinken@gmail.com>
2021-12-19 23:10:58 +0530

    Home page added

---
+++ public/js/home.js
@@ -0,0 +1,21 @@
+
+// First slider
+$('.slider-one').not(".slick-intialized").slick({
+    dots: true,
+    // infinite: true,
+    // speed: 700,
+    autoplay:true,
+    autoplaySpeed: 3000,
+    prevArrow:".site-slider .slider-btn .prev",
+    nextArrow:".site-slider .slider-btn .next",
+})
+
+// Category slider
+// $('.slider-two').not(".slick-intialized").slick({
+//     prevArrow:".site-slider-two .slider-btn .prev",
+//     nextArrow:".site-slider-two .slider-btn .next",
+//     slidesToShow: 5,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed:3000
+// })
