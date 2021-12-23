22426185b62395978e71a3eff5cd42f88678d579
linkenvashistha <vashisthalinken@gmail.com>
2021-12-23 14:38:32 +0530

    F

---
+++ public/js/search.js
@@ -0,0 +1,12 @@
+const myForm = document.getElementById('search-form')
+
+myForm.addEventListener('submit',(e) => {
+    e.preventDefault();
+    const keyword = document.getElementById('inputSearch').value;
+    if (keyword.trim()) {
+      window.location.assign(window.location.pathname+`/product/${keyword}`);
+    } else {
+      window.location.assign(window.location.pathname+`/product`);
+    }
+    console.log(keyword);
+});
